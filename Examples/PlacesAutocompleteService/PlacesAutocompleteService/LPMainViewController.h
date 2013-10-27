2e82dbbe5889b2e7b02b3ea202dca2aca41812d0
Luka Penger <luka.penger@gmail.com>
2013-10-27 15:07:10 +0100

    fix

---
+++ Examples/PlacesAutocompleteService/PlacesAutocompleteService/LPMainViewController.h
@@ -0,0 +1,18 @@
+//
+//  LPMainViewController.h
+//  PlacesAutocomplateService
+//
+//  Created by Luka Penger on 8/21/13.
+//  Copyright (c) 2013 Luka Penger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LPGoogleFunctions.h"
+
+@interface LPMainViewController : UIViewController <LPGoogleFunctionsDelegate>
+
+@property (nonatomic, strong) LPGoogleFunctions *googleFunctions;
+    
+@property (nonatomic, strong) IBOutlet UITextView *textView;
+    
+@end
---
+++ Examples/PlacesAutocompleteService/PlacesAutocompleteService/LPMainViewController.m
@@ -0,0 +1,81 @@
+//
+//  LPMainViewController.m
+//  PlacesAutocomplateService
+//
+//  Created by Luka Penger on 8/21/13.
+//  Copyright (c) 2013 Luka Penger. All rights reserved.
+//
+
+#import "LPMainViewController.h"
+
+@interface LPMainViewController ()
+
+@end
+
+@implementation LPMainViewController
+
+NSString *const googleAPIBrowserKey = @"AIzaSyCi7y2eH4jZWTnoUYLIGeDqSCEmD3-FxNU";
+
+- (void)viewDidLoad
+{
+    [self.googleFunctions loadPlacesAutocompleteForInput:@"New York" offset:0 radius:0 location:nil placeType:LPGooglePlaceTypeGeocode countryRestriction:nil successfulBlock:^(LPPlacesAutocomplete *placesAutocomplete) {
+        
+        NSLog(@"successful");
+        
+        self.textView.text = placesAutocomplete.description;
+        
+        if(placesAutocomplete.predictions.count>0)
+        {
+            LPPrediction *prediction = (LPPrediction*)[placesAutocomplete.predictions objectAtIndex:0];
+            
+            NSString *reference = prediction.reference;
+            
+            [self.googleFunctions loadPlaceDetailsForReference:reference successfulBlock:^(LPPlaceDetailsResults *placeDetailsResults) {
+                
+                NSLog(@"successful load details");
+                
+                NSLog(@"%@",placeDetailsResults.description);
+                
+            } failureBlock:^(LPGoogleStatus status) {
+                
+                NSLog(@"Error loading details - Block: %@",[LPGoogleFunctions getGoogleStatus:status]);
+                
+            }];
+        }
+        
+    } failureBlock:^(LPGoogleStatus status) {
+        
+        NSLog(@"Error - Block: %@",[LPGoogleFunctions getGoogleStatus:status]);
+        
+    }];
+}
+
+- (LPGoogleFunctions*)googleFunctions {
+    if(!_googleFunctions) {
+        _googleFunctions = [LPGoogleFunctions new];
+        _googleFunctions.googleAPIBrowserKey = googleAPIBrowserKey;
+        _googleFunctions.delegate = self;
+        _googleFunctions.sensor = YES;
+        //_googleFunctions.languageCode = "en" // set language
+    }
+    return _googleFunctions;
+}
+
+#pragma mark - LPGoogleFunctions Delegate Methods
+
+- (void)googleFunctionsWillLoadPlacesAutocomplate:(LPGoogleFunctions *)googleFunctions forInput:(NSString *)input
+{
+    NSLog(@"willLoadPlacesAutcompleteForInput: %@",input);
+}
+    
+- (void)googleFunctions:(LPGoogleFunctions *)googleFunctions didLoadPlacesAutocomplate:(LPPlacesAutocomplete *)placesAutocomplate
+{
+    NSLog(@"didLoadPlacesAutocomplete - Delegate");
+}
+
+- (void)googleFunctions:(LPGoogleFunctions *)googleFunctions errorLoadingPlacesAutocomplateWithStatus:(LPGoogleStatus)status
+{
+    NSLog(@"errorLoadingPlacesAutocomplateWithStatus - Delegate: %@",[LPGoogleFunctions getGoogleStatus:status]);
+}
+
+@end
